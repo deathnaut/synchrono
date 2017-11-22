d9ec1a2e74c67b17a5a763142fefbc65d39f9ef3
Karla Moreno <moreno_karla@live.com>
2017-11-22 13:42:26 -0600

    got sound playing on button click

--- src/components/button.js
+++ src/components/button.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SoundFile from './sound.js';
 import './button.css';
 
 class Button extends Component {
@@ -8,6 +9,8 @@ class Button extends Component {
     this.state = {
       bgColor: 'yellow',
       active: false,
+      boxShadow: 'none',
+      sounds: [],
     }
     this._handleClick = this._handleClick.bind(this);
   }
@@ -18,11 +21,14 @@ class Button extends Component {
       this.setState({
         bgColor: 'blue',
         active: true,
+        sounds: this.state.sounds.concat({ url: '' })
+        // boxShadow: '0 0 20px #000'
       })
     } else {
       this.setState({
         bgColor: 'yellow',
         active: false,
+        boxShadow: 'none',
       })
     }
   }
@@ -30,7 +36,8 @@ class Button extends Component {
   render() {
     return (
       <div className="button">
-        <button name="button" onClick={ this._handleClick } style={{backgroundColor:this.state.bgColor}}>i am a button</button>
+        <button name="button" onClick={ this._handleClick } style={{backgroundColor:this.state.bgColor, boxShadow:this.state.boxShadow}}>i am a button</button>
+        {this.state.sounds.map((sound) => <SoundFile url={sound.url}/>)}
       </div>
     );
   }
